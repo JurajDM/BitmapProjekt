d6d53805932392e412afe3dcd880735a3ae84ca0
unknown <juraj.d.maglica@gmail.com>
2022-03-04 13:38:08 +0100

    first

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+
+namespace BitmapProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var color = new Bitmap("C:/Users/Reroot/source/repos/BitmapProjekt/BitmapProjekt/bitmap/hamburg.bmp");
+            var bw = color.Clone(new Rectangle(0, 0, color.Width, color.Height), PixelFormat.Format1bppIndexed);
+            bw.Save("C:/Users/Reroot/source/repos/BitmapProjekt/BitmapProjekt/bitmap/hamburg_bw.bmp", ImageFormat.Bmp);
+          
+        }
+       
+
+    }
+}
+
